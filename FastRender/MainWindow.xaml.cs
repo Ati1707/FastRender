cc6b9c6cd6517746e078ff05831d8f18c37dbb0a
Ati1707 <atikiro1300@gmail.com>
2024-03-11 11:52:50 +0100

    added basic volume and time slider. Not happy with the time slider yet it feels not responsive enough

--- FastRender/MainWindow.xaml.cs
+++ FastRender/MainWindow.xaml.cs
@@ -19,12 +19,13 @@ namespace FastRender
 	{
 		public List<Video> VideoList = new List<Video>();
 		private bool isDragging;
+		private bool isDragCompleted;
 		private Point startPoint;
 		public MainWindow()
 		{
 			InitializeComponent();
-			System.IO.Directory.CreateDirectory("Images");
-			System.IO.Directory.CreateDirectory("Videos");
+			Directory.CreateDirectory("Images");
+			Directory.CreateDirectory("Videos");
 			videoListBox.ItemsSource = VideoList;
 
 		}
@@ -47,13 +48,13 @@ namespace FastRender
 			{
 				string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
 
-                // Handle the dropped files (add them to your VideoList, for example)
-                foreach (var file in files)
+				// Handle the dropped files (add them to your VideoList, for example)
+				foreach (var file in files)
 				{
-                    // Add logic to check if the file is a video file and get its information
-                    // For demonstration purposes, let's assume you have a method called AddVideoFromFile
-                    // that adds a video to the VideoList based on the file path.
-                    AddVideoFromFile(file);
+					// Add logic to check if the file is a video file and get its information
+					// For demonstration purposes, let's assume you have a method called AddVideoFromFile
+					// that adds a video to the VideoList based on the file path.
+					AddVideoFromFile(file);
 				}
 			}
 		}
@@ -66,7 +67,7 @@ namespace FastRender
 			{
 				if (obj.GetType() == typeof(ListBoxItem))
 				{
-					mediaElement.Source = new System.Uri(((sender as ListBox).SelectedItem as Video).VideoPath);
+					mediaElement.Source = new Uri(((sender as ListBox).SelectedItem as Video).VideoPath);
 					break;
 				}
 				obj = VisualTreeHelper.GetParent(obj);
@@ -86,20 +87,20 @@ namespace FastRender
 			// Add logic to extract video information and add it to your VideoList
 			// For demonstration purposes, let's assume you have a Video class with VideoThumbnail and VideoTitle properties.
 			var videoInfo = VideoInformationExtractorHelper.VidExtractor(filepath);
-			VideoList.Add(new Video { VideoThumbnail = videoInfo.Item1, VideoTitle = videoInfo.Item2, VideoDuration = videoInfo.Item3 , VideoPath = filepath});
+			VideoList.Add(new Video { VideoThumbnail = videoInfo.Item1, VideoTitle = videoInfo.Item2, VideoDuration = videoInfo.Item3, VideoPath = filepath });
 
 			videoListBox.Items.Refresh();
 
 		}
-		
-			void Window_Closing(object sender, CancelEventArgs e)
+
+		void Window_Closing(object sender, CancelEventArgs e)
+		{
+			System.IO.DirectoryInfo imageDirectory = new DirectoryInfo("Images");
+			foreach (FileInfo file in imageDirectory.GetFiles())
 			{
-				System.IO.DirectoryInfo imageDirectory = new DirectoryInfo("Images");
-				foreach (FileInfo file in imageDirectory.GetFiles())
-				{
-					file.Delete();
-				}
+				file.Delete();
 			}
+		}
 
 
 		private void videoListBox_PreviewMouseMove(object sender, MouseEventArgs e)
@@ -108,7 +109,7 @@ namespace FastRender
 			{
 				ListBox box = sender as ListBox;
 				Video video = box.SelectedItem as Video;
-				DragDrop.DoDragDrop(box,video, DragDropEffects.Copy);
+				DragDrop.DoDragDrop(box, video, DragDropEffects.Copy);
 			}
 		}
 
@@ -120,7 +121,8 @@ namespace FastRender
 		private void Rectangle_Drop(object sender, DragEventArgs e)
 		{
 			object data = e.Data.GetData(typeof(Video));
-			if (data != null) { 
+			if (data != null)
+			{
 				Video video = data as Video;
 				Image thumbnail = new Image();
 				thumbnail.Source = new BitmapImage(new Uri(video.VideoThumbnail));
@@ -129,15 +131,42 @@ namespace FastRender
 			}
 		}
 
+
+		private void ChangeMediaVolume(object sender, RoutedPropertyChangedEventArgs<double> args)
+		{
+			mediaElement.Volume = (double)volumeSlider.Value;
+		}
+
+		private void SeekToMediaPosition()
+		{
+			int SliderValue = (int)timelineSlider.Value;
+			if (mediaElement.NaturalDuration.HasTimeSpan)
+			{
+				var mediaDuration = mediaElement.NaturalDuration.TimeSpan.TotalMilliseconds;
+				SliderValue = (int)(timelineSlider.Value * mediaDuration) / 100;
+			}
+			Debug.WriteLine(SliderValue);
+			// Overloaded constructor takes the arguments days, hours, minutes, seconds, milliseconds.
+			// Create a TimeSpan with miliseconds equal to the slider value.
+
+			TimeSpan ts = new TimeSpan(0, 0, 0, 0, SliderValue);
+			mediaElement.Position = ts;
+			isDragCompleted = false;
+		}
+
+		private void timelineSlider_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+		{
+			SeekToMediaPosition();
+		}
 	}
+
 	public class Video
 	{
 
 		public required string VideoThumbnail { get; set; }
 		public required string VideoTitle { get; set; }
 		public required string VideoDuration { get; set; }
-		public required string VideoPath {  get; set; }
+		public required string VideoPath { get; set; }
 		public override string ToString() => this.VideoTitle;
 	}
-
 }
